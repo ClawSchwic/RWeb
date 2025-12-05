1a4345c4fc0d8f2c2e0a603bf1af6431d23ee83e
Claw the Dragon <clawschwic@gmail.com>
2025-12-05 12:00:06 -0600

    Added functions
    Added a Send and Receive base functions.

--- RWeb.rb
+++ RWeb.rb
@@ -2,13 +2,22 @@
 require 'socket'
 
 class RWeb
-  attr_accessor :IPAddress, :Port, :Server
-  
+  attr_accessor :IPAddress, :Port
+  attr :Server
+
   def initialize(ip_address, port)
     self.IPAddress = ip_address
     self.Port = port
 
-    self.Server = TCPServer.new(ip_address, port)
+    @Server = TCPServer.new(ip_address, port)
+  end
+
+  def Send(data)
+    
+  end
+
+  def Receive()
+    
   end
 
   def Close()
